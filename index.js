81cf5043d3dd5dfeb1902e2193fb616be810a086
Tomokuni Kani <mameyurirails@gmail.com>
2017-01-24 05:19:00 +0900

    first commit

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-var express = require('express');
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-
+const https = require('https');
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const options = {
+  key: fs.readFileSync('crypto/server.key'),
+  cert: fs.readFileSync('crypto/server.crt')
+};
+https.createServer(options, app).listen(443);
