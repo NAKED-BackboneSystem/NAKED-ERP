5526303908e4e637d2336162379ae24f77b25a22
Tomokuni Kani <mameyurirails@gmail.com>
2017-01-24 10:47:24 +0900

    Add Post Request Callback

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
-var https = require('https');
-var fs = require('fs');
 var express = require('express');
 var app = express();
-var options = {
-  key: fs.readFileSync('crypto/server.key'),
-  cert: fs.readFileSync('crypto/server.crt')
-};
+
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
-app.post('/data', function(req, res){
-  console.log('JSON:');
-  res.jsonp(null);
-  // res.json({
-  //   "speech": "Barack Hussein Obama II is the 44th and current President of the United States.",
-  //   "displayText": "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
-  //   "data": {},
-  //   "contextOut": [],
-  //   "source": "DuckDuckGo"
-  // });
-});
-app.set('port', (process.env.PORT || 5000));
-https.createServer(options, app).listen(app.get('port'), function() {
+
+app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
