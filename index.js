964443e1c4b3fbe5386d214adcbae4f8103860a1
Tomokuni Kani <mameyurirails@gmail.com>
2017-01-24 12:58:02 +0900

    Add Post Request Callback

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.post('/data', function(req, res){
   res.json({
      "speech": "Barack Hussein Obama II is the 44th and current President of the United States.",
      "displayText": "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
-     "data": {},
+     "data": {"slack": {}},
      "contextOut": [],
      "source": "DuckDuckGo"
   });
