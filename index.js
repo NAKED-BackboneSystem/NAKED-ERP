55360f309bf0378df5e06c64dc25e702079ae35f
Tomokuni Kani <mameyurirails@gmail.com>
2017-01-25 11:17:55 +0900

    Add Post Request Callback

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
+// This is the Implementations of the Node.js Database Servers.
+
+// Load Modules
+// Express (Node.js Web Application Framework)
 var express = require('express');
 var app = express();
 
+// Set Listen Ports
 app.set('port', (process.env.PORT || 5000));
 
+// Set Assets Directory
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
+// Set Template Engines
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
+// Route HTTP Requests
+// GET from /
+app.get('/', function(req, res){
   res.render('pages/index');
 });
+// POST to /data
 app.post('/data', function(req, res){
-  res.json({
-    "speech": "Barack Hussein Obama II is the 44th and current President of the United States.",
-    "displayText": "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
-    "data": {"slack": {
-      "text": "This is a line of text.\nAnd this is another one."
-    }},
-    "contextOut": [],
-    "source": "DuckDuckGo"
-  });
+  res.json(
+    {
+      "speech": "This is the Reply Text for API.AI.",
+      "displayText": "This is the Reply Text for API.AI.",
+      "data": {
+        "slack": {
+          "text": "This is the Reply Text for Slack."
+        }
+      },
+      "contextOut": [],
+      "source": "Database Server"
+    }
+  );
 });
 
-app.listen(app.get('port'), function() {
+// Start Servers
+app.listen(app.get('port'), function(){
   console.log('Node app is running on port', app.get('port'));
 });
