d057eef72405a39f121bb1a9b956c9310a762d3b
Tomokuni Kani <mameyurirails@gmail.com>
2017-01-25 10:40:30 +0900

    Add Post Request Callback

--- index.js
+++ index.js
@@ -16,39 +16,7 @@ app.post('/data', function(req, res){
   res.json({
     "speech": "Barack Hussein Obama II is the 44th and current President of the United States.",
     "displayText": "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
-    "data": {"slack": {
-    "text": "Barack Hussein Obama II is the 44th and current President of the United States.",
-    "attachments": [
-        {
-            "title": channel.get('title'),
-            "title_link": channel.get('link'),
-            "color": "#36a64f",
-
-            "fields": [
-                {
-                    "title": "Condition",
-                    "value": "Temp " + condition.get('temp') +
-                             " " + units.get('temperature'),
-                    "short": "false"
-                },
-                {
-                    "title": "Wind",
-                    "value": "Speed: " + channel.get('wind').get('speed') +
-                             ", direction: " + channel.get('wind').get('direction'),
-                    "short": "true"
-                },
-                {
-                    "title": "Atmosphere",
-                    "value": "Humidity " + channel.get('atmosphere').get('humidity') +
-                             " pressure " + channel.get('atmosphere').get('pressure'),
-                    "short": "true"
-                }
-            ],
-
-            "thumb_url": "http://l.yimg.com/a/i/us/we/52/" + condition.get('code') + ".gif"
-        }
-    ]
-}},
+    "data": {"slack": {}},
     "contextOut": [],
     "source": "DuckDuckGo"
   });
